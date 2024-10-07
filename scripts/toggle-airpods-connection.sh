18b684ef678fab043ed45240de2f1f26f2eca448
Julia Mertz <info@juliamertz.dev>
2024-10-07 20:01:13 +0200

    delete old scripts

--- scripts/toggle-airpods-connection.sh
+++ scripts/toggle-airpods-connection.sh
@@ -4,11 +4,10 @@ MAC_ADDRESS="2C:32:6A:D6:B4:B5"
 STATUS=$(bluetoothctl info $MAC_ADDRESS)
 FALLBACK_DEVICE="alsa_output.usb-Logitech_G735_Gaming_Headset-01.iec958-stereo"
 
-
 if echo $STATUS | grep -q "Connected: yes"; then
     bluetoothctl disconnect $MAC_ADDRESS
     pactl set-default-sink $FALLBACK_DEVICE
 else
     bluetoothctl connect $MAC_ADDRESS
-    pactl set-default-sink bluez_output.$(echo $MAD_ADDRESS | tr '_' ':').1
+    pactl set-default-sink bluez_output.$(echo $MAC_ADDRESS | tr '_' ':').1
 fi  
--- scripts/wake-pc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-
-timeout 50 bash -c "while true; do if ping -c1 -i1 8.8.8.8 &>/dev/null; then echo "up"; break; fi; done"
-
