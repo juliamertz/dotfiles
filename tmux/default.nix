68b35c21c0bf2f7e00d057af9bd9e42db4afc295
Julia Mertz <info@juliamertz.dev>
2024-11-19 16:10:50 +0100

    tmux: re-enable theme

--- tmux/default.nix
+++ tmux/default.nix
@@ -38,12 +38,12 @@ let
         set -g @plugin 'tmux-plugins/tmux-sensible'
         set -g @plugin 'tmux-plugins/tmux-yank'
         set -g @plugin 'sainnhe/tmux-fzf'
-        # set -g @plugin 'rose-pine/tmux'
+        set -g @plugin 'rose-pine/tmux'
 
         # Theme
-        # set -g @rose_pine_variant 'moon'
-        # set -g @rose_pine_bar_bg_disable 'on'
-        # set -g @rose_pine_bar_bg_disabled_color_option 'default'
+        set -g @rose_pine_variant 'moon'
+        set -g @rose_pine_bar_bg_disable 'on'
+        set -g @rose_pine_bar_bg_disabled_color_option 'default'
         set -g status-bg default
         set -g status-style bg=default
 
