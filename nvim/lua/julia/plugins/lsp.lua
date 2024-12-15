276adeccf6127ffd810c1e46bb97533c579d10d3
Julia Mertz <info@juliamertz.dev>
2024-12-15 01:07:27 +0100

    nvim: fix duplicate bind

--- nvim/lua/julia/plugins/lsp.lua
+++ nvim/lua/julia/plugins/lsp.lua
@@ -35,7 +35,6 @@ return {
 			lsp.on_attach(function(_, bufnr)
 				local opts = { buffer = bufnr, remap = false }
 
-				vim.keymap.set('n', '<leader>vrr', vim.lsp.buf.references, opts)
 				vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
 				vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 				vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float, opts)
