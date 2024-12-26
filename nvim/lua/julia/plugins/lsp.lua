5848aac62754a129a2185d8271f1426355c958ba
Julia Mertz <info@juliamertz.dev>
2024-12-26 18:06:32 +0100

    nvim: fix problems with blink.cmp

--- nvim/lua/julia/plugins/lsp.lua
+++ nvim/lua/julia/plugins/lsp.lua
@@ -16,7 +16,7 @@ return {
 		'neovim/nvim-lspconfig',
 		dependencies = { 'saghen/blink.cmp' },
 
-		opts = { servers = {  } },
+		opts = { servers = {} },
 
 		config = function(_, options)
 			local lspconfig = require 'lspconfig'
@@ -86,10 +86,21 @@ return {
 			{ 'rafamadriz/friendly-snippets' },
 		},
 
-		version = 'v0.7.6',
+		version = '*',
 
 		opts = {
-			keymap = { preset = 'default' },
+			keymap = {
+				preset = 'none',
+				['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+				['<C-e>'] = { 'hide' },
+				['<C-y>'] = { 'select_and_accept' },
+
+				['<C-p>'] = { 'select_prev', 'fallback' },
+				['<C-n>'] = { 'select_next', 'fallback' },
+
+				['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+				['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+			},
 			completion = {
 				documentation = {
 					auto_show = true,
