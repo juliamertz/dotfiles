4c112388c817cc03e24eb9e696f99f7734b5e467
Julia Mertz <info@juliamertz.dev>
2025-01-14 01:22:32 +0100

    cleanup

--- nvim/default.nix
+++ nvim/default.nix
@@ -1,7 +1,4 @@
 {
-  ripgrep,
-  stdenv,
-
   vimPlugins,
   inputs,
   system,
@@ -9,42 +6,11 @@
   ...
 }:
 let
-  nixvim' = inputs.nixvim.legacyPackages.${system};
+  nixvim = inputs.nixvim.legacyPackages.${system};
   nixvimModule = {
     inherit pkgs;
     module = import ./config.nix;
     extraSpecialArgs = { inherit vimPlugins; };
   };
-
-  # plugins =
-  #   linkFarmFromDrvs "neovim-plugins"
-  #   <| (with vimPlugins; [
-  #     rose-pine
-  #     harpoon2
-  #     telescope-nvim
-  #     conform-nvim
-  #     snacks-nvim
-  #     trouble-nvim
-  #     lazydev-nvim
-  #     nvim-tree-lua
-  #     oil-nvim
-  #     noice-nvim
-  #     nui-nvim
-  #     ccc-nvim
-  #     plenary-nvim
-  #     render-markdown-nvim
-  #     todo-comments-nvim
-  #     mini-statusline
-  #     mini-surround
-  #     mini-git
-  #     mini-icons
-  #     (nvim-treesitter.withPlugins (
-  #       plugins: with plugins; [
-  #         nix
-  #         lua
-  #       ]
-  #     ))
-  # ]);
-
 in
-nixvim'.makeNixvimWithModule nixvimModule
+nixvim.makeNixvimWithModule nixvimModule
