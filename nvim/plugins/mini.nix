b5940dd26f7c4a8db319585100ff74e6f329490c
Julia Mertz <info@juliamertz.dev>
2025-01-26 00:02:20 +0100

    nvim: show macro recording msg in statusline

--- nvim/plugins/mini.nix
+++ nvim/plugins/mini.nix
@@ -1,22 +1,11 @@
 {
   plugins.mini = {
     enable = true;
-
-    # emulate nvim-web-devicons for plugins that don’t natively support it.
     mockDevIcons = true;
 
     modules = {
       icons = { };
       git = { };
-
-      statusline = {
-        content = {
-          active = null;
-          inactive = null;
-        };
-        use_icons = true;
-        set_vim_settings = true;
-      };
 
       surround = {
         mappings = {
