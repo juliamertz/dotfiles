959d1667fbdb61e719de6079916f65374a411786
Julia Mertz <info@juliamertz.dev>
2025-01-27 19:05:18 +0100

    clean up kitty configuration

--- kitty/default.nix
+++ kitty/default.nix
@@ -1,40 +1,43 @@
 {
-  wrapPackage,
   writeText,
+  fetchFromGitHub,
+  runCommandNoCC,
+  wrapPackage,
+
   kitty,
+  imagemagick,
   ...
 }:
 let
-  config =
-    writeText "kitty.conf" # sh
+  theme = fetchFromGitHub {
+    owner = "rose-pine";
+    repo = "kitty";
+    rev = "788bf1bf1a688dff9bbacbd9e516d83ac7dbd216";
+    hash = "sha256-AcMVkliLGuabZVGkfQPLhfspkaTZxPG5GyuJdzA4uSg=";
+  };
+
+  kittyConf = writeText "kitty.conf" (
+    (builtins.readFile ./kitty.conf) + "include ${theme}/dist/rose-pine-moon.conf"
+  );
+
+  configDir =
+    runCommandNoCC "kitty-config-directory"
+      {
+        nativeBuildInputs = [ imagemagick ];
+      }
       ''
-        include ${./themes/rose-pine-moon.conf}
+        mkdir $out
+        cp ${kittyConf} $out/kitty.conf
+        cp ${theme}/icons/kitty.*.png $out
 
-        font_size 22.00
-        font_family JetBrainsMono Nerd Font Mono
-
-        hide_window_decorations titlebar-only
-        window_padding_width 10 10 0 10
-        tab_bar_style fade
-        tab_title_template {index}
-        background_opacity 0.85
-
-        map alt+1 goto_tab 1
-        map alt+2 goto_tab 2
-        map alt+3 goto_tab 3
-        map alt+4 goto_tab 4
-        map alt+5 goto_tab 5
-        map alt+6 goto_tab 6
-        map alt+7 goto_tab 7
-        map alt+8 goto_tab 8
-        map alt+9 goto_tab 9
-        map alt+t launch --cwd=current --type=tab
-        map alt+w close_tab
-        map ctrl+shift+escape no_op
+        # resize app icon for x11 
+        magick ${theme}/icons/kitty.app.png -resize 128x128 $out/kitty.app-128.png
       '';
 in
 wrapPackage {
   name = "kitty";
   package = kitty;
-  extraFlags = "--config ${config}";
+  extraArgs = [
+    "--set KITTY_CONFIG_DIRECTORY '${configDir}'"
+  ];
 }
