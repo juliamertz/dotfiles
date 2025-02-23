3de1e03583f6a324640191507ddde455e7dbd002
Julia Mertz <info@juliamertz.dev>
2025-02-23 14:17:23 +0100

    wip zebar widgets
    cleanup

--- zebar/default.nix
+++ zebar/default.nix
@@ -2,11 +2,23 @@
   inputs,
   system,
   wrapPackage,
+  callPackage,
   ...
 }:
+let
+  lib = callPackage ./lib.nix { };
+  config = lib.mkConfig ./widgets {
+    startupConfigs = [
+      {
+        path = "bar/bar.zebar.json";
+        preset = "default";
+      }
+    ];
+  };
+in
 wrapPackage {
   package = inputs.zebar.packages.${system}.default;
   extraArgs = [
-    "--set ZEBAR_CONFIG '${./widgets}'"
+    "--set ZEBAR_CONFIG '${config}'"
   ];
 }
