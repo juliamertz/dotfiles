cc51715991c164da2d8814eb9e0d04988962a92c
Julia Mertz <info@juliamertz.dev>
2025-03-07 04:00:34 +0100

    organize scripts
    clean
    
    clean
    
    clean
    
    updates

--- scripts/default.nix
+++ scripts/default.nix
@@ -1,46 +1,127 @@
 {
+  pkgs,
   lib,
-  runCommandNoCC,
   writeShellScriptBin,
   symlinkJoin,
-  readNixFiles,
-  callPackage,
+  runCommand,
   packages,
   ...
 }:
 let
-  mkScript =
-    names: content:
-    if builtins.isList names then
-      let
-        name = builtins.elemAt names 0;
-        script = writeShellScriptBin name content;
-        aliases = builtins.filter (n: n != name) names;
-        linkAlias =
-          alias: # sh
-          ''
-            ln -vs $out/bin/${name} $out/bin/${alias}
-          '';
-      in
-      runCommandNoCC "script-${name}" { } ''
-        mkdir -p $out/bin
-        cp ${script}/bin/${name} $out/bin/${name}
-        ${map (a: linkAlias a) aliases |> lib.concatStringsSep "\n"}
-      ''
-    else
-      let
-        script = writeShellScriptBin names content;
-      in
-      runCommandNoCC "script-${names}" { } ''
-        mkdir -p $out/bin
-        cp ${script}/bin/${names} $out/bin/${names}
-      '';
+  inherit (lib.strings) splitString;
+  inherit (builtins)
+    elemAt
+    match
+    filter
+    isNull
+    ;
 
-  files = readNixFiles ./.;
-  localPath = filename: ./. + "/${filename}";
-  scripts = map (p: callPackage (localPath p) { inherit mkScript packages; }) files;
+  scriptPackages = pkgs // {
+    dotfiles = packages;
+  };
+
+  parseNixShellShebang =
+    args:
+    let
+      # TODO: make these regexes more forgiving
+      # working format: '#! nix-shell -i bash -p pkgs.neovim
+      interpreter = elemAt (match ".*-i ([a-zA-Z]+).*" args) 0;
+      packages = elemAt (match ".*-p (.*)" args) 0 |> splitString " ";
+    in
+    {
+      inherit
+        interpreter
+        packages
+        ;
+    };
+
+  parseRequirements =
+    content:
+    splitString "\n" content
+    |> map (
+      line:
+      let
+        parts = splitString " " line;
+      in
+      if (elemAt parts 0) == "#!" && (elemAt parts 1) == "nix-shell" then
+        parts |> lib.drop 2 |> lib.concatStringsSep " " |> parseNixShellShebang
+      else if (elemAt parts 0) == "#?" && (elemAt parts 1) == "alias" then
+        { aliases = parts |> lib.drop 1; }
+      else
+        null
+    )
+    |> filter (value: !isNull value)
+    |> lib.flatten
+    |> lib.mergeAttrsList;
+
+  lookupAttrpath =
+    attrs: path:
+    let
+      next = lib.getAttr (elemAt path 0) attrs;
+    in
+    if lib.length path == 1 then next else lookupAttrpath (next) (lib.drop 1 path);
+
+  deps2binPath =
+    deps:
+    let
+      packages = map (p: lookupAttrpath scriptPackages (lib.splitString "." p)) deps;
+    in
+    (lib.makeBinPath packages) + ":$PATH";
+
+  readScripts =
+    path:
+    let
+      isScript = name: name != "default.nix";
+      toPath = (filename: ./. + "/${filename}");
+      files = builtins.readDir path;
+    in
+    filter isScript (builtins.attrNames files) |> map toPath;
+
+  cleanShebangs =
+    content:
+    let
+      lines = lib.splitString "\n" content;
+      isMetadata = line: builtins.match "#[!?].*" line |> isNull;
+    in
+    builtins.filter isMetadata lines |> lib.concatStringsSep "\n";
+
+  mkAliasBin =
+    script: alias:
+    runCommand "alias" { } ''
+      mkdir -p $out/bin
+      ln -sf ${lib.getExe script} $out/bin/${alias}
+    '';
+
+  wrapScript =
+    {
+      path,
+      requirements,
+      useDash ? false,
+    }:
+    let
+      binPath = if lib.hasAttr "packages" requirements then deps2binPath requirements.packages else null;
+      patched = pkgs.writeScript "${builtins.baseNameOf path}-patched" ''
+        #!${lib.getExe pkgs.${requirements.interpreter or "bash"}}
+        ${cleanShebangs (builtins.readFile path)}
+      '';
+    in
+    writeShellScriptBin (builtins.baseNameOf path) ''
+      ${lib.optionalString (!isNull binPath) ''export PATH="${binPath}"''}
+      exec ${patched} "$@"
+    '';
 in
 symlinkJoin {
   name = "scripts";
-  paths = scripts;
+  paths =
+    readScripts ./.
+    |> map (
+      path:
+      let
+        requirements = parseRequirements (builtins.readFile path);
+        wrapped = wrapScript { inherit path requirements; };
+        aliases = map (a: mkAliasBin wrapped a) (requirements.aliases or [ ]);
+      in
+      [ wrapped ] ++ aliases
+    )
+    |> lib.flatten;
 }
