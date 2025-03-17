42a3cfdbb4a2ff08b1f1fdc945b7ef4b45cf360f
Julia Mertz <info@juliamertz.dev>
2025-03-17 21:35:32 +0100

    nvim: disable filetree notifications

--- nvim/plugins/filetree.nix
+++ nvim/plugins/filetree.nix
@@ -10,6 +10,9 @@
       renderer = {
         group_empty = true;
       };
+      notify = {
+        threshold.__raw = "vim.log.levels.ERROR";
+      };
       filters = {
         dotfiles = false;
       };
