3fa8271feaab29ebae1e86038588936c378bb087
Julia Mertz <info@juliamertz.dev>
2025-03-30 06:30:21 +0200

    cleanup tmux config
    clean
    
    clean
    
    cleanup

---
+++ tmux/builder.nix
@@ -0,0 +1,84 @@
+{
+  lib,
+  writeText,
+  runCommandNoCC,
+  stdenvNoCC,
+  makeWrapper,
+  tmux,
+  ...
+}:
+rec {
+  makeTmuxConfig =
+    {
+      # Global variables
+      globals ? { },
+      # Environment variables
+      environment ? { },
+      # List of tmux plugin packages
+      plugins ? [ ],
+      # Global plugin options to be prefixed by a '@'
+      pluginOptions ? { },
+      # Source these tmux conf files at runtime
+      sourceFiles ? [ ],
+      # Extra raw tmux config
+      extraConfig ? "",
+      # Reset XDG_CONFIG_HOME back to "$HOME/.config" after initialization
+      patchXdgConfig ? false,
+      ...
+    }:
+    let
+      inherit (lib) mapAttrsToList optionalString;
+      joinLines = lib.concatStringsSep "\n";
+      setGlobal = key: value: "set -g ${key} ${value}";
+      setEnv = key: value: "set-environment -g ${key} ${value}";
+      sourcePlugin = p: "run-shell ${p}/share/tmux-plugins/${p.pluginName}/${p.pluginName}.tmux";
+
+      sourceFilesText = map (path: "source-file ${path}") sourceFiles |> joinLines;
+      setGlobalsText = mapAttrsToList setGlobal globals |> joinLines;
+      setPluginOptionsText = mapAttrsToList (k: v: setGlobal "@${k}" v) pluginOptions |> joinLines;
+      sourcePluginsText = map sourcePlugin plugins |> joinLines;
+      setEnvironmentText = mapAttrsToList setEnv environment |> joinLines;
+      patchXdgConfigHomeText = optionalString patchXdgConfig <| setEnv "XDG_CONFIG_HOME" "$HOME/.config";
+    in
+    writeText "tmux.conf" (joinLines [
+      sourceFilesText
+      setGlobalsText
+      setPluginOptionsText
+      sourcePluginsText
+      setEnvironmentText
+      extraConfig
+      patchXdgConfigHomeText
+    ]);
+
+  makeTmuxConfigDir =
+    opts:
+    runCommandNoCC (opts.name or "tmux-config") { } ''
+      mkdir -p $out/tmux
+      ln -svf ${makeTmuxConfig opts} $out/tmux/tmux.conf
+    '';
+
+  makeTmuxBin =
+    opts:
+    let
+      config = makeTmuxConfigDir opts;
+      package = opts.package or tmux;
+    in
+    stdenvNoCC.mkDerivation {
+      name = "tmux";
+      src = package;
+      buildInputs = [ makeWrapper ];
+
+      buildPhase = ''
+        mkdir -p $out/bin
+        ln -s ${package}/bin/tmux $out/bin/tmux
+      '';
+
+      installPhase = ''
+        wrapProgram $out/bin/tmux \
+          --set XDG_CONFIG_HOME '${config}' \
+          --add-flags "-f ${config}/tmux/tmux.conf" 
+      '';
+
+      meta.mainProgram = "tmux";
+    };
+}
--- tmux/default.nix
+++ tmux/default.nix
@@ -1,51 +1,37 @@
 {
   lib,
-  tmux,
   tmuxPlugins,
-  wrapPackage,
-  runCommandNoCC,
-  writeText,
+  callPackage,
   packages,
   ...
 }:
 let
-  sourcePlugin = p: "run-shell ${p}/share/tmux-plugins/${p.pluginName}/${p.pluginName}.tmux";
-
+  builder = callPackage ./builder.nix { };
   shell = lib.getExe packages.zsh;
-  tmuxConf =
-    writeText "tmux.conf" # sh
-      ''
-        source-file ${./tmux.reset.conf}
-
-        set -g default-shell ${shell}
-        set -g default-command ${shell}
-        set -g @sessionx-zoxide-mode 'on'
-        set -g prefix ^A
-
-        bind-key -r f run-shell "tmux neww ${packages.scripts}/bin/sessionizer"
-
-        set -g @rose_pine_variant 'moon'
-        set -g @rose_pine_bar_bg_disable 'on'
-        set -g @rose_pine_bar_bg_disabled_color_option 'default'
-        set -g status-bg default
-        set -g status-style bg=default
-
-        ${sourcePlugin tmuxPlugins.sensible}
-        ${sourcePlugin tmuxPlugins.yank}
-        ${sourcePlugin tmuxPlugins.rose-pine}
-
-        set-environment -g XDG_CONFIG_HOME "$HOME/.config"
-      '';
-
-  config = runCommandNoCC "tmux-conf" { } ''
-    mkdir -p $out/tmux
-    cp -r ${tmuxConf} $out/tmux/tmux.conf
-  '';
 in
-wrapPackage {
-  package = tmux;
-  # If XDG_CONFIG_HOME isn't set to the tmux config directory most plugins won't work
-  # this should overriden back to the users home after initialization
-  extraArgs = "--set XDG_CONFIG_HOME '${config}'";
-  extraFlags = "-f ${config}/tmux/tmux.conf";
+builder.makeTmuxBin {
+  sourceFiles = [ ./tmux.conf ];
+
+  globals = {
+    default-shell = "${shell}";
+    default-command = "${shell}";
+  };
+
+  plugins = with tmuxPlugins; [
+    sensible
+    yank
+    rose-pine
+  ];
+  pluginOptions = {
+    rose_pine_variant = "moon";
+    rose_pine_bar_bg_disable = "on";
+    rose_pine_bar_bg_disabled_color_option = "default";
+    sessionx-zoxide-mode = "on";
+  };
+
+  patchXdgConfig = true;
+  extraConfig = # sh
+    ''
+      bind-key -r f run-shell "tmux neww ${packages.scripts}/bin/sessionizer"
+    '';
 }
