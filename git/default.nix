cceef9935fdeac89609d08bb1c2c52e3b6720365
Julia Mertz <info@juliamertz.dev>
2025-04-24 00:05:16 +0200

    add extra work git config for commit signing
    oopsie
    
    cleanup
    
    fix
    
    cleanup
    
    fix

--- git/default.nix
+++ git/default.nix
@@ -1,17 +1,46 @@
 {
+  lib,
   wrapPackage,
-  runCommandNoCC,
+  writeText,
   git,
+  #
+  signingKey ? null,
+  use1Password ? false,
   ...
 }: let
-  configDir = runCommandNoCC "git-config" {} ''
-    mkdir -vp $out/git
-    ln -svf ${./config} $out/git/config
-  '';
+  makeGitconfig = {
+    package ? git,
+    configPath,
+    extraConfig ? "",
+  }: let
+    gitConfig =
+      writeText ".gitconfig"
+      <| lib.concatStringsSep "\n" [
+        (builtins.readFile configPath)
+        (builtins.readFile "${package}/etc/gitconfig")
+        extraConfig
+      ];
+  in
+    wrapPackage {
+      inherit package;
+      extraFlags = ["-c 'include.path=${gitConfig}'"];
+    };
+
+  configPath = ./.gitconfig;
 in
-  wrapPackage {
-    package = git;
-    extraArgs = [
-      "--set XDG_CONFIG_HOME '${configDir}'"
-    ];
+  makeGitconfig {
+    inherit configPath;
+    extraConfig =
+      {
+        user = {inherit signingKey;};
+        commit.gpgsign = true;
+        gpg = {
+          format = "ssh";
+          ssh = lib.optionalAttrs use1Password {
+            program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+          };
+        };
+      }
+      |> lib.optionalAttrs (builtins.isString signingKey)
+      |> lib.generators.toGitINI;
   }
