aabb9c5aa6c663475ec03f996b879bbf45accd96
Julia Mertz <info@juliamertz.dev>
2025-05-15 19:32:07 +0200

    fix: use upstream aerospace package

--- aerospace/default.nix
+++ aerospace/default.nix
@@ -3,17 +3,9 @@
   fetchzip,
   aerospace,
   ...
-}: let
-  package = aerospace.overrideAttrs rec {
-    version = "0.18.4-Beta";
-    src = fetchzip {
-      url = "https://github.com/nikitabobko/AeroSpace/releases/download/v${version}/AeroSpace-v${version}.zip";
-      sha256 = "sha256-TjVxq1HS/gdGi32noj7i1P6e9lXKNtBoO373Cesnwks=";
-    };
-  };
-in
-  wrapPackage {
-    inherit package;
-    wrapPaths = ["/Applications/AeroSpace.app/Contents/MacOS/AeroSpace"];
-    extraFlags = "--config-path ${./config.toml}";
-  }
+}:
+wrapPackage {
+  package = aerospace;
+  wrapPaths = ["/Applications/AeroSpace.app/Contents/MacOS/AeroSpace"];
+  extraFlags = "--config-path ${./config.toml}";
+}
