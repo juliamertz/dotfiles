231a53bcb70179e66d013cb9086892d59ff0e38a
juliamertz <35079666+juliamertz@users.noreply.github.com>
2025-06-30 16:48:37 +0000

    run treefmt

--- emacs/default.nix
+++ emacs/default.nix
@@ -3,26 +3,28 @@
   wrapPackage,
   ...
 }:
-  wrapPackage {
-    package = emacs.pkgs.withPackages epkgs:
-    with epkgs; [
-      use-package
-      general
-      evil
-      evil-markdown
-      catppuccin-theme
-      magit
+wrapPackage {
+  package =
+    emacs.pkgs.withPackages
+    epkgs:
+      with epkgs; [
+        use-package
+        general
+        evil
+        evil-markdown
+        catppuccin-theme
+        magit
 
-      # language support
-      markdown-mode
-      lsp-mode
-      lsp-ui
-      rustic
-      nix-mode
-      nixfmt
-    ];
-    extraFlags = [
-      "--load ${./init.el}"
-      # "--init-directory=${./.}"
-    ];
-  }
+        # language support
+        markdown-mode
+        lsp-mode
+        lsp-ui
+        rustic
+        nix-mode
+        nixfmt
+      ];
+  extraFlags = [
+    "--load ${./init.el}"
+    # "--init-directory=${./.}"
+  ];
+}
