fcdb60dc53c65a226c244a3a265dd02e75767c1f
Julia Mertz <info@juliamertz.dev>
2025-07-19 22:08:02 +0200

    fix emacs

--- emacs/default.nix
+++ emacs/default.nix
@@ -4,25 +4,23 @@
   ...
 }:
 wrapPackage {
-  package =
-    emacs.pkgs.withPackages
-    epkgs:
-      with epkgs; [
-        use-package
-        general
-        evil
-        evil-markdown
-        catppuccin-theme
-        magit
+  package = emacs.pkgs.withPackages (epkgs:
+    with epkgs; [
+      use-package
+      general
+      evil
+      evil-markdown
+      catppuccin-theme
+      magit
 
-        # language support
-        markdown-mode
-        lsp-mode
-        lsp-ui
-        rustic
-        nix-mode
-        nixfmt
-      ];
+      markdown-mode
+      lsp-mode
+      lsp-ui
+      rustic
+      nix-mode
+      nixfmt
+    ]);
+
   extraFlags = [
     "--load ${./init.el}"
     # "--init-directory=${./.}"
