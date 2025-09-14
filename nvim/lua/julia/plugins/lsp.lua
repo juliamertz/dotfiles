4cfd28e86ea91fcbeef89c77d72eb96057a512aa
Julia Mertz <info@juliamertz.dev>
2025-09-14 14:17:21 +0200

    nvim: migrate to `0.11.x` builtin lsp config

--- nvim/lua/julia/plugins/lsp.lua
+++ nvim/lua/julia/plugins/lsp.lua
@@ -3,89 +3,13 @@ local utils = require 'utils'
 return {
 	{
 		'neovim/nvim-lspconfig',
-		dependencies = {
-			{
-				'williamboman/mason.nvim',
-				enabled = utils.lazyAdd(true, false),
-				config = true,
-			}, -- NOTE: Must be loaded before dependants
-			{
-				'williamboman/mason-lspconfig.nvim',
-				enabled = utils.lazyAdd(true, false),
-			},
-			{
-				'WhoIsSethDaniel/mason-tool-installer.nvim',
-				enabled = utils.lazyAdd(true, false),
-			},
 
-			-- LSP Status messages
-			-- { "j-hui/fidget.nvim", opts = {} },
-		},
 		config = function()
-			vim.api.nvim_create_autocmd('LspAttach', {
-				group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-				callback = function(event)
-					local builtin = require 'telescope.builtin'
-					local map = function(keys, func, desc)
-						vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-					end
-					map('gd', builtin.lsp_definitions, '[G]oto [D]efinition')
-					map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-					map('gr', builtin.lsp_references, '[G]oto [R]eferences')
-					map('gI', builtin.lsp_implementations, '[G]oto [I]mplementation')
-					map('<leader>D', builtin.lsp_type_definitions, 'Type [D]efinition')
-					map('<leader>ds', builtin.lsp_document_symbols, '[D]ocument [S]ymbols')
-					map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-					map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-					map('K', vim.lsp.buf.hover, 'Hover Documentation')
-
-					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if client and client.server_capabilities.documentHighlightProvider then
-						local highlight_augroup =
-							vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-						vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.document_highlight,
-						})
-
-						vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.clear_references,
-						})
-
-						vim.api.nvim_create_autocmd('LspDetach', {
-							group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-							callback = function(event2)
-								vim.lsp.buf.clear_references()
-								vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-							end,
-						})
-					end
-
-					if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-						map('<leader>th', function()
-							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-						end, '[T]oggle Inlay [H]ints')
-					end
-				end,
-			})
-
-			local configs = require 'lspconfig.configs'
-			local util = require 'lspconfig.util'
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			local lsp_utils = require 'lspconfig.util'
 
 			local servers = {}
 
-			if utils.enableForCat 'nix' then
-				if utils.isNixCats then
-					servers.nixd = {}
-				else
-					servers.rnix = {}
-					servers.nil_ls = {}
-				end
-			end
+			servers.nixd = utils.ifCat('nix', {})
 
 			servers.bashls = utils.ifCat('shell', {
 				cmd = { 'bash-language-server', 'start' },
@@ -135,15 +59,8 @@ return {
 			servers.gopls = utils.ifCat('go', {})
 
 			if utils.enableForCat 'javascript' then
-				servers.denols = {
-					on_attach = on_attach,
-					root_dir = require('lspconfig').util.root_pattern('deno.json', 'deno.jsonc'),
-				}
-				-- servers.ts_ls = {
-				-- 	on_attach = on_attach,
-				-- 	root_dir = require('lspconfig').util.root_pattern 'package.json',
-				-- 	single_file_support = false,
-				-- }
+				servers.denols = { root_dir = lsp_utils.root_pattern('deno.json', 'deno.jsonc') }
+				servers.ts_ls = { root_dir = lsp_utils.root_pattern 'package.json' }
 
 				servers.astro = {}
 				servers.svelte = {
@@ -185,15 +102,13 @@ return {
 				},
 			})
 
-			configs.protobuf_language_server = {
-				default_config = {
-					cmd = { 'protobuf-language-server' },
-					filetypes = { 'proto', 'cpp' },
-					root_dir = util.root_pattern '.git',
-					single_file_support = true,
-					settings = {
-						['additional-proto-dirs'] = '',
-					},
+			servers.protobuf_language_server = {
+				cmd = { 'protobuf-language-server' },
+				filetypes = { 'proto', 'cpp' },
+				root_dir = lsp_utils.root_pattern '.git',
+				single_file_support = true,
+				settings = {
+					['additional-proto-dirs'] = '',
 				},
 			}
 
@@ -201,36 +116,72 @@ return {
 			servers.prismals = {}
 
 			if utils.isNixCats then
-				for server_name, config in pairs(servers) do
-					require('lspconfig')[server_name].setup {
+				for name, config in pairs(servers) do
+					vim.lsp.enable(name)
+					vim.lsp.config(name, {
+						settings = config.settings or {},
+						root_pattern = config.root_pattern,
+						filetypes = config.filetypes,
+						cmd = config.cmd,
 						capabilities = vim.tbl_deep_extend(
 							'force',
-							capabilities,
+							vim.lsp.protocol.make_client_capabilities(),
 							require('blink.cmp').get_lsp_capabilities(config.capabilities)
 						),
-						settings = (servers[server_name] or {}).settings,
-						filetypes = (servers[server_name] or {}).filetypes,
-						cmd = (servers[server_name] or {}).cmd,
-						root_pattern = (servers[server_name] or {}).root_pattern,
-					}
+					})
 				end
-			else
-				local ensure_installed = vim.tbl_keys(servers or {})
-				vim.list_extend(ensure_installed, { 'stylua' })
-
-				require('mason').setup()
-				require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-				require('mason-lspconfig').setup {
-					handlers = {
-						function(server_name)
-							local server = servers[server_name] or {}
-							server.capabilities =
-								vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-							require('lspconfig')[server_name].setup(server)
-						end,
-					},
-				}
 			end
+
+			vim.api.nvim_create_autocmd('LspAttach', {
+				group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+				callback = function(event)
+					local telescope = require 'telescope.builtin'
+					local map = function(keys, func, desc)
+						vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+					end
+
+					map('gd', telescope.lsp_definitions, '[G]oto [D]efinition')
+					map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+					map('gr', telescope.lsp_references, '[G]oto [R]eferences')
+					map('gI', telescope.lsp_implementations, '[G]oto [I]mplementation')
+					map('<leader>D', telescope.lsp_type_definitions, 'Type [D]efinition')
+					map('<leader>ds', telescope.lsp_document_symbols, '[D]ocument [S]ymbols')
+					map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+					map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+					map('K', vim.lsp.buf.hover, 'Hover Documentation')
+
+					local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+					if client and client.server_capabilities.documentHighlightProvider then
+						local highlight_group = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
+						vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+							buffer = event.buf,
+							group = highlight_group,
+							callback = vim.lsp.buf.document_highlight,
+						})
+
+						vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+							buffer = event.buf,
+							group = highlight_group,
+							callback = vim.lsp.buf.clear_references,
+						})
+
+						vim.api.nvim_create_autocmd('LspDetach', {
+							group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
+							callback = function(event2)
+								vim.lsp.buf.clear_references()
+								vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
+							end,
+						})
+					end
+
+					if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+						map('<leader>th', function()
+							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+						end, '[T]oggle Inlay [H]ints')
+					end
+				end,
+			})
 		end,
 	},
 
@@ -245,7 +196,5 @@ return {
 		},
 	},
 
-	{
-		'kcl-lang/kcl.nvim',
-	},
+	{ 'kcl-lang/kcl.nvim' },
 }
