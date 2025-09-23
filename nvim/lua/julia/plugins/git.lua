0191f4c509097e3175127ea25522bcd14ca9494a
Julia Mertz <info@juliamertz.dev>
2025-09-24 01:50:40 +0200

    nvim: update gitsigns config
    amend

--- nvim/lua/julia/plugins/git.lua
+++ nvim/lua/julia/plugins/git.lua
@@ -27,15 +27,27 @@ return {
 	{
 		'lewis6991/gitsigns.nvim',
 		enabled = utils.enableForCat 'git',
+
 		opts = {
-			signs = {
-				add = { text = '+' },
-				change = { text = '~' },
-				delete = { text = '_' },
-				topdelete = { text = '‾' },
-				changedelete = { text = '~' },
-			},
+      signs = {
+        add          = { text = '┃' },
+        change       = { text = '┃' },
+        delete       = { text = '_' },
+        topdelete    = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked    = { text = '┆' },
+      },
+      signs_staged = {
+        add          = { text = '┃' },
+        change       = { text = '┃' },
+        delete       = { text = '_' },
+        topdelete    = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked    = { text = '┆' },
+      },
 		},
+
+    lazy = false,
 
 		keys = {
 			{ '<leader>gb', '<cmd>Gitsigns blame<cr>', desc = 'Open git blame' },
