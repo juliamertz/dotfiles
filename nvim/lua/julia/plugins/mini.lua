8da9e82adc490b246efe6dcf38368ed8b8ef10f4
Julia Mertz <info@juliamertz.dev>
2025-09-26 20:59:04 +0200

    nvim: integrate statusline into tmux bar
    fix

--- nvim/lua/julia/plugins/mini.lua
+++ nvim/lua/julia/plugins/mini.lua
@@ -23,60 +23,10 @@ return {
 
 	{
 		'echasnovski/mini.icons',
+		lazy = false,
 		config = function()
 			local icons = require 'mini.icons'
 			icons.mock_nvim_web_devicons()
-		end,
-	},
-
-	{
-		'echasnovski/mini.statusline',
-		opts = {
-			content = {
-				inactive = nil,
-				active = function()
-					local check_macro_recording = function()
-						if vim.fn.reg_recording() ~= '' then
-							return 'Recording @' .. vim.fn.reg_recording()
-						else
-							return ''
-						end
-					end
-
-					local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
-					local git = MiniStatusline.section_git { trunc_width = 40 }
-					local diff = MiniStatusline.section_diff { trunc_width = 75 }
-					local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75 }
-					local filename = MiniStatusline.section_filename { trunc_width = 140 }
-					local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 }
-					local location = MiniStatusline.section_location { trunc_width = 200 }
-					local search = MiniStatusline.section_searchcount { trunc_width = 75 }
-					local macro = check_macro_recording()
-
-					return MiniStatusline.combine_groups {
-						{ hl = mode_hl, strings = { mode } },
-						{ hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics } },
-						'%<', -- Mark general truncate point
-						{ hl = 'MiniStatuslineFilename', strings = { filename } },
-						'%=', -- End left alignment
-						{ hl = 'MiniStatuslineFilename', strings = { macro } },
-						{ hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
-						{ hl = mode_hl, strings = { search, location } },
-					}
-				end,
-			},
-			use_icons = nixCats 'have_nerd_font',
-		},
-		init = function()
-			-- Disable statusline for certain filetypes
-			vim.api.nvim_create_autocmd('Filetype', {
-				callback = function(args)
-					local disabled_filetypes = { 'man', 'NvimTree', 'oil' }
-					if vim.tbl_contains(disabled_filetypes, vim.bo[args.buf].filetype) then
-						vim.b[args.buf].ministatusline_disable = true
-					end
-				end,
-			})
 		end,
 	},
 }
---
+++ nvim/lua/julia/plugins/statusline.lua
@@ -0,0 +1,80 @@
+return {
+	{
+		'echasnovski/mini.statusline',
+		dependencies = { 'echasnovski/mini.icons' },
+		opts = {
+			content = {
+				inactive = nil,
+				active = function()
+					local icons = require 'mini.icons'
+
+					local check_macro_recording = function()
+						if vim.fn.reg_recording() ~= '' then
+							return 'Recording @' .. vim.fn.reg_recording()
+						else
+							return ''
+						end
+					end
+
+					local section_fileinfo = function()
+						local filetype = vim.bo.filetype
+						local icon = icons.get('filetype', filetype)
+						local text = ''
+
+						if filetype ~= '' and icon ~= nil then
+							return icon .. ' ' .. filetype
+						end
+
+						if icon ~= nil then
+							return icon
+						end
+
+						return filetype
+					end
+
+					local _, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
+					local fileinfo = section_fileinfo()
+					local macro = check_macro_recording()
+					local location = MiniStatusline.section_location { trunc_width = 120 }
+
+					return MiniStatusline.combine_groups {
+						'%<', -- Mark general truncate point
+						'%=', -- End left alignment
+						{ hl = 'MiniStatuslineFilename', strings = { macro } },
+						{ hl = 'MiniStatuslineLocation', strings = { location } },
+						{ hl = mode_hl, strings = { fileinfo } },
+					}
+				end,
+			},
+			use_icons = nixCats 'have_nerd_font',
+		},
+		init = function()
+			-- Disable statusline for certain filetypes
+			vim.api.nvim_create_autocmd('Filetype', {
+				callback = function(args)
+					local disabled_filetypes = { 'man', 'NvimTree', 'oil' }
+					if vim.tbl_contains(disabled_filetypes, vim.bo[args.buf].filetype) then
+						vim.b[args.buf].ministatusline_disable = true
+					end
+				end,
+			})
+		end,
+	},
+
+	{
+		'vimpostor/vim-tpipeline',
+		init = function()
+			vim.g.tpipeline_embedopts = {
+				[[ status-right '#(cat #{socket_path}-\#{session_id}-vimbridge-R)' ]],
+				[[ status-right-length 99 ]],
+			}
+
+			if vim.env.TMUX then
+				vim.opt.laststatus = 0
+				vim.g.tpipeline_autoembed = 1
+      else
+				vim.g.tpipeline_autoembed = 0
+			end
+		end,
+	},
+}
